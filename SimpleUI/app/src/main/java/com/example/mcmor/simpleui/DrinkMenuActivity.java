9768dbcd11ff8fef1be43b2fbd8bfa25a1bf399c
pibomb <pibomb988@gmail.com>
2016-08-11 00:34:05 -0400

    Add Request Codes
    Used request codes to communicate between MainActivity and
    DrinkMenuActivity.

--- SimpleUI/app/src/main/java/com/example/mcmor/simpleui/DrinkMenuActivity.java
+++ SimpleUI/app/src/main/java/com/example/mcmor/simpleui/DrinkMenuActivity.java
@@ -1,25 +1,29 @@
 package com.example.mcmor.simpleui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class DrinkMenuActivity extends AppCompatActivity {
 
     ListView drinkMenuListView;
     TextView totalTextView;
 
-
     String[] names = {"冬瓜紅茶", "玫瑰鹽奶蓋紅茶", "珍珠紅茶拿鐵", "紅茶拿鐵"};
     int[] lPrices = {35, 45, 55, 45};
     int[] mPrices = {25, 35, 45, 35};
     int[] imageIds = {R.drawable.drink1, R.drawable.drink2, R.drawable.drink3, R.drawable.drink4};
 
+    int total = 0;
     List<Drink> drinkList = new ArrayList<Drink>();
 
     @Override
@@ -31,6 +35,15 @@ public class DrinkMenuActivity extends AppCompatActivity {
 
         drinkMenuListView = (ListView) findViewById(R.id.drinkMenuListView);
         totalTextView = (TextView) findViewById(R.id.totalTextView);
+
+        drinkMenuListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Drink drink = (Drink) parent.getAdapter().getItem(position);
+                total += drink.mPrice;
+                totalTextView.setText(String.valueOf(total));
+            }
+        });
 
         setupDrinkMenu();
 
@@ -51,6 +64,14 @@ public class DrinkMenuActivity extends AppCompatActivity {
     private void setupDrinkMenu() {
         DrinkAdapter adapter = new DrinkAdapter(this, drinkList);
         drinkMenuListView.setAdapter(adapter);
+    }
+
+    public void done(View view) {
+        Intent intent = new Intent();
+        intent.putExtra("result", String.valueOf(total));
+
+        setResult(RESULT_OK, intent);
+        finish();
     }
 
     @Override
--- SimpleUI/app/src/main/java/com/example/mcmor/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/mcmor/simpleui/MainActivity.java
@@ -22,6 +22,8 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final int REQUEST_CODE_DRINK_MENU_ACTIVITY = 0;
+
     TextView textView;
     EditText editText;
     RadioGroup radioGroup;
@@ -123,8 +125,19 @@ public class MainActivity extends AppCompatActivity {
     public void goToMenu(View view) {
         Intent intent = new Intent();
         intent.setClass(this, DrinkMenuActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, REQUEST_CODE_DRINK_MENU_ACTIVITY);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CODE_DRINK_MENU_ACTIVITY) {
+            if(resultCode == RESULT_OK) {
+                String result = data.getStringExtra("result");
+                Toast.makeText(this, result, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
